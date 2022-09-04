5a827d14e9f417e5fe49d2dbcd99a87d6ddd221d
abdullahAljuhi <95518730+abdullahAljuhi@users.noreply.github.com>
2022-09-04 14:18:08 +0300

    fixed referance to order in migration

--- database/migrations/2022_08_13_174331_create_payments_table.php
+++ database/migrations/2022_08_13_174331_create_payments_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->string('image')->nullable();
-            $table->unsignedBigInteger('order_id');
-            $table->foreign('order_id')->references('id')->on('orders')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('order_id');           
+            $table->unsignedBigInteger('payment_type_id');
+            $table->foreign('payment_type_id')->references('id')->on('payment_types')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
